f62b247c205819af128d6cb19018ddf404f3f455
aureobordignon <52795001+aureobordignon@users.noreply.github.com>
2020-10-20 17:02:45 -0300

    s

---
+++ react-native-redux/todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='About' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Esta é a nossa história...</p>
+        <h2>Missão</h2>
+        <p>Esta é a nossa missão...</p>
+    </div>
+)
